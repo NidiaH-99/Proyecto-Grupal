d580c7cff23dd4043429ad8916963395378c0690
NidiaH-99 <nidia281@hotmail.com>
2025-10-27 16:56:00 -0600

    Actualizacion de codigo semana 6

--- app/src/main/java/cr/ac/utn/census/PersonActivity.kt
+++ app/src/main/java/cr/ac/utn/census/PersonActivity.kt
@@ -1,8 +1,8 @@
 package cr.ac.utn.census
 
+import Controller.PersonController
 import Entity.Person
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -11,7 +11,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import cr.ac.utn.census.R.id.TxtEmail_Person
 
 class PersonActivity : AppCompatActivity() {
     private lateinit var txtId: EditText
@@ -35,7 +34,7 @@ class PersonActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-            txtId= findViewById<EditText>(R.id.TxtID_Person)
+            txtId= findViewById<EditText>(R.id.TxtId_Person)
             txtName= findViewById<EditText>(R.id.TxtName_Person)
             txtLastName= findViewById<EditText>(R.id.TxtLastName_Person)
             txtSLastName= findViewById<EditText>(R.id.TxtSLastName_Person)
@@ -55,16 +54,36 @@ class PersonActivity : AppCompatActivity() {
 
     }
 
-    fun validationData(person): Boolean{
+    fun validationData(person: Person): Boolean{
         return true
     }
 
     fun savePerson(){
         try {
             val person = Person()
-            person ID = txt.text.toString()
+            person.ID = txtId.text.toString()
+            person.Name = txtName.text.toString()
+            person.FLastName = txtLastName.text.toString()
+            person.SLastName = txtSLastName.text.toString()
+            person.Email = txtEmail.text.toString()
+            person.Phone = txtPhone.text.toString().toInt()
+            //person.Birthday = txtBirthdate.text.toString().
+            //person.Province = txtProvince.text.toString()
+            person.State = txtState.text.toString()
+            person.District = txtDistrict.text.toString()
+            person.Address= txtAddress.text.toString()
+            if (validationData(person)){
+                val personController= PersonController(this)
+                personController.addPerson(person)
+                Toast.makeText(this, "La persona se guardò"
+                    , Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(this, "Datos incompletos"
+                    , Toast.LENGTH_LONG).show()
+            }
         }catch (e: Exception){
-            Toast.makeText(this, e.message.toString(), Toast.LENGTH_LONG).show()
+            Toast.makeText(this, e.message.toString()
+                , Toast.LENGTH_LONG).show()
         }
     }
 }
